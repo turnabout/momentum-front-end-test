eaac758eb2aeb6e21d0a8aca8f2acd26bdcd2a32
Kevin <kevin@kangamedia.com>
2016-10-05 13:00:38 -0400

    Add part of login functionality, check username existence

--- app/js/main.js
+++ app/js/main.js
@@ -43,11 +43,34 @@ var momentumModule = (function momentumModule() {
 	 * @param {Event} event - The event.
 	 */
 	function login(event) {
-		var username = document.getElementById('login-field').value;
+		var loginField = document.getElementById('login-field');
+		var loginAlert = document.getElementById('login-alert');
+		var loginBox = document.getElementById('login-box');
+		var loginBtn = document.getElementById('login-btn');
+		var username = loginField.value;
 
 		if(username) {
+
+			// Disable the form
+			loginField.setAttribute('disabled', 'disabled');
+			loginBtn.setAttribute('disabled', 'disabled');
+			
 			getJSON(`http://jsonplaceholder.typicode.com/users?username=${username}`, function (result) {
-				console.log(result);
+				if(result.length > 0) {
+					// Username exists, clear any previous error and log in
+					user = result[0];
+				} else {
+					// Username does not exist, display error message
+					loginAlert.innerHTML = `Username "${username}" does not exist.`;
+					loginAlert.classList.add('active');
+					loginBox.classList.add('error');
+
+					// Enable form and focus back on field
+					loginField.removeAttribute('disabled', 'disabled');
+					loginBtn.removeAttribute('disabled', 'disabled');
+					loginField.focus();
+
+				}
 			});
 		}
 
