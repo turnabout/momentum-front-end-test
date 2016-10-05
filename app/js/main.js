fc91dd8740ee4f5605db215c39cafad74d30dc15
Kevin <kevin@kangamedia.com>
2016-10-05 12:31:37 -0400

    Tie login function to login form submit

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,22 @@
-var momentumModule = (function () {
+/**
+ * Module containing the code for the Momentum Front End app.
+ * @return {Object} publicApi - Api containing references to the module functions.
+ */
+var momentumModule = (function momentumModule() {
 	
+	var user = {};
+
+
+	/**
+	 * Initialize everything.
+	 */
+	 function init() {
+	 	// Attach login function to login form
+	 	document.getElementById('login').addEventListener('submit', login);
+
+
+	 }
+
 	/**
 	 * Get JSON data.
 	 * @param {String} url - The URL to fetch the JSON from.
@@ -22,21 +39,27 @@ var momentumModule = (function () {
 	}
 
 	/**
-	* Attempt to log user in.
-	* @param {String} username - The user's name.
-	* @return {Boolean} - Whether the login is successful.
-	*/
-	function login(username) {
-		getJSON(`http://jsonplaceholder.typicode.com/users?username=${username}`, function (result) {
-			console.log(result);
-		});
+	 * Attempt to log user in on form submit.
+	 * @param {Event} event - The event.
+	 */
+	function login(event) {
+		var username = document.getElementById('login-field').value;
+
+		if(username) {
+			getJSON(`http://jsonplaceholder.typicode.com/users?username=${username}`, function (result) {
+				console.log(result);
+			});
+		}
+
+		event.preventDefault();
 	}
 
 	return {
 		'getJSON' : getJSON,
+		'init' : init,
 		'login' : login
 	};
 
 })();
 
-momentumModule.login('Bret');
+momentumModule.init();
